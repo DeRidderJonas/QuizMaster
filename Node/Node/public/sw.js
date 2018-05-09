17bb89269abe2b97f5bdbf9f59d3300b4dff21fa
Unknown <jonas.de.ridder@student.howest.be>
2018-05-09 07:21:29 +0200

    Updated files are used on second request

--- Node/Node/public/sw.js
+++ Node/Node/public/sw.js
@@ -28,11 +28,18 @@ self.addEventListener('install', function (event) {
     )
 });
 
-self.addEventListener('fetch', function (event) { //works offline but doesn't update files
+self.addEventListener('fetch', function (event) { //works offline but updated files are used second request
     //console.log("SW fetch: ", event.request);
     if(!(event.request.url.indexOf('getAnyQuiz') > -1) && !(event.request.url.indexOf('getQuestionsForQuiz') > -1)){
         event.respondWith(
             caches.match(event.request).then(function (res) {
+                if(res){
+                    fetch(event.request).then(function (response) {
+                        caches.open('v1').then(function (cache) {
+                            cache.put(event.request, response);
+                        })
+                    })
+                }
                 return res || fetch(event.request).then(function (response) {
                     return caches.open('v1').then(function (cache) {
                         cache.put(event.request, response.clone());
@@ -43,26 +50,6 @@ self.addEventListener('fetch', function (event) { //works offline but doesn't up
         )
     }
 });
-
-
-/*
-
-self.addEventListener('fetch', function (event) { //gives updated files but doesn't work offline
-    if(!(event.request.url.indexOf('getAnyQuiz') > -1) && !(event.request.url.indexOf('getQuestionsForQuiz') > -1)){
-        event.respondWith(
-            caches.open('v1').then(function (cache) {
-                return fetch(event.request).then(function (response) {
-                    cache.put(event.request, response);
-                    return response;
-                }) || caches.match(event.request).then(function (res) {
-                    return res;
-                })
-            })
-        )
-    }
-});
-*/
-
 
 self.addEventListener('activate', function (event) {
     console.log("SW activate");
