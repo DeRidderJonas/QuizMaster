416c889e4498a1b78764e8fe94c7dd0174a13969
Unknown <jonas.de.ridder@student.howest.be>
2018-05-09 07:03:55 +0200

    update files with sw (no offline)
    file updating version of sw in comment because it doens't work offline

--- Node/Node/public/sw.js
+++ Node/Node/public/sw.js
@@ -28,9 +28,9 @@ self.addEventListener('install', function (event) {
     )
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function (event) { //works offline but doesn't update files
     //console.log("SW fetch: ", event.request);
-    if(!(event.request.url.indexOf('getAnyQuiz') > -1)){
+    if(!(event.request.url.indexOf('getAnyQuiz') > -1) && !(event.request.url.indexOf('getQuestionsForQuiz') > -1)){
         event.respondWith(
             caches.match(event.request).then(function (res) {
                 return res || fetch(event.request).then(function (response) {
@@ -43,6 +43,19 @@ self.addEventListener('fetch', function (event) {
         )
     }
 });
+
+/*self.addEventListener('fetch', function (event) { //gives updated files but doesn't work offline
+    if(!(event.request.url.indexOf('getAnyQuiz') > -1) && !(event.request.url.indexOf('getQuestionsForQuiz') > -1)){
+        event.respondWith(
+            caches.open('v1').then(function (cache) {
+                return fetch(event.request).then(function (response) {
+                    cache.put(event.request, response);
+                    return response;
+                })
+            })
+        )
+    }
+});*/
 
 self.addEventListener('activate', function (event) {
     console.log("SW activate");
