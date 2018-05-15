744919eff467f98ef8409745faac4859cb9f2877
Unknown <jonas.de.ridder@student.howest.be>
2018-05-15 17:54:20 +0200

    reading quizzes on server succes, shared-js broken

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/assets/js",express.static(path.join(__dirname, 'shared-javascript')));
+app.use("/assets/js/",express.static(path.join(__dirname, 'shared-javascript')));
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 
